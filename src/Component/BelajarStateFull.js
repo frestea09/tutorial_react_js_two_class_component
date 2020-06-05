7def5115716733e13c6476473757a3fd5c0f7b6f
frestea09 <ilmanfrasetya@gmail.com>
2020-06-05 14:35:19 +0700

    first commit

---
+++ src/Component/BelajarStateFull.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class BelajarStateFull extends React.Component{
+    render(){
+        return(
+            <p>Hello Nama saya ilman teguh prasetya</p>
+        )
+    }
+}
+export default BelajarStateFull
---
+++ src/Component/BelajarStateless.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const BelajarStateless = () =>{
+    return(
+        <h3>Hello World</h3>
+    );
+}
+export default BelajarStateless
